919166c48e420cd65136634d023ba65103177fe3
LujanSolo <lujansolo@proton.me>
2024-05-29 11:02:01 -0700

    corrected path error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default {
   root: resolve(__dirname, 'src'),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
     }
   },
   build: {
